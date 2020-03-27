7d3c2ce18962513ed4664782ace5c63d01f26bc5
hhui64 <907322015@qq.com>
2020-03-28 01:12:03 +0800

    style: 添加 LocalTitle 类成员注释

--- src/main/java/io/github/hhui64/titlex/Ttitle/LocalTitle.java
+++ src/main/java/io/github/hhui64/titlex/Ttitle/LocalTitle.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class LocalTitle extends Title {
-  public ConfigurationSection configurationSection;
+  /**
+   * 本地称号的配置节点
+   */
+  public ConfigurationSection c;
 
-  public LocalTitle(ConfigurationSection configurationSection, String id, String container, String value,
-      List<String> profile, double oneDayPrice, double permanentPrice, int minBuyDays, int maxBuyDays) {
-    super(id, container, value, profile, oneDayPrice, permanentPrice, minBuyDays, maxBuyDays);
-    this.configurationSection = configurationSection;
+  public LocalTitle(ConfigurationSection c, String id, String container, String value,
+      List<String> profile, double oneDayPrice, double permanentPrice, int minBuyDays, int maxBuyDays, boolean isCanBuy) {
+    super(id, container, value, profile, oneDayPrice, permanentPrice, minBuyDays, maxBuyDays, isCanBuy);
+    this.c = c;
   }
 }
